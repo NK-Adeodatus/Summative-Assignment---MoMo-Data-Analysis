2de48ed68efe10eb596138ddcb3d5774e03ee6ca
N-Adeodatus <nkundiman3@gmail.com>
2025-06-11 19:18:06 +0200

    design the cards

--- back_end/api.py
+++ back_end/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response 
 import sqlite3
 from extract import extract_sms
 import os
@@ -10,34 +10,12 @@ app = Flask(__name__)
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(SCRIPT_DIR, 'MoMo_SMS.db')
 
-# Check if the data has already been extracted from the XML file to the dadabase
-# def check_extraction():
-#     print('check run')
-#     if not os.path.exists(DB_PATH):
-#         print('check run in if')
-#         extract_sms()
-#         return
-#     try:
-#         print('check run in try')
-#         conn = sqlite3.connect(DB_PATH)
-#         cursor = conn.cursor()
-#         cursor.execute('SELECT COUNT(*) FROM sms')
-#         count = cursor.fetchone()[0]
-#         conn.close()
-#         if count == 0:
-#             extract_sms()
-#     except sqlite3.Error as e:
-#         print("SQLite error:", e)
-#         extract_sms()
-
-
 # Get the sms data from the database
 def get_all_sms():
-    # check_extraction()
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
 
-    cursor.execute('SELECT type_of_sms, amount, sender, receiver, message, new_balance, time FROM sms LIMIT 1')
+    cursor.execute('SELECT type_of_sms, amount, sender, receiver, message, new_balance, time FROM sms LIMIT 5')
     rows = cursor.fetchall()
     conn.close()
 
@@ -57,7 +35,10 @@ def get_all_sms():
 # serve the data from database in json format.
 @app.route('/api/sms', methods=['GET'])
 def api_sms():
-    return jsonify(get_all_sms())
+    response = make_response(jsonify(get_all_sms()))
+    response.headers['Access-Control-Allow-Origin'] = '*' 
+    return response
+    # return jsonify(get_all_sms())
 
 def run_app():
     app.run(debug=True)
--- front_end/index.js
+++ front_end/index.js
@@ -7,14 +7,31 @@ let messages = fetch('http://127.0.0.1:5000/api/sms')
             card.classList.add('sms-card');
 
             card.innerHTML = `
-                ${sms.type && `<h3>${sms.type}</h3>`}
-                ${sms.sender && `<p><strong>Sender:</strong> ${sms.sender}</p>`}
-                ${sms.amount != null && `<p><strong>Amount:</strong> ${sms.amount} RWF</p>`}
-                ${sms.time && `<p><strong>Time:</strong> ${sms.time}</p>`}
-                ${sms.message && `<p><strong>Message:</strong> ${sms.message}</p>`}
-                ${sms.new_balance != null && `<p><strong>New Balance:</strong> ${sms.new_balance} RWF</p>`}
+                <div class='info-container'>
+                ${sms.type  ? `<h3>${sms.type}</h3>` : ''}
+                ${sms.sender  ? `<p><strong>Sender:</strong> ${sms.sender}</p>` : ''}
+                ${sms.receiver  ? `<p><strong>receiver:</strong> ${sms.receiver}</p>` : ''}
+                ${sms.amount  ? `<p><strong>Amount:</strong> ${sms.amount} RWF</p>` : ''}
+                ${sms.new_balance != null ? `<p><strong>New Balance:</strong> ${sms.new_balance} RWF</p>` : ''}
+                ${sms.time != null ? `<p><strong>Time:</strong> ${sms.time}</p>` : ''}
+                </div>
+                ${sms.message ? `<div class='message-container hidden'><strong>Message:</strong> ${sms.message}</div>` : ''}
+                <button class='show-btn btn'>Show SMS</button>
+                <button class='hide-btn btn hidden'>Hide SMS</button>
             `;
-
+            const message_container = card.querySelector('.message-container')
+            const show_sms_btn = card.querySelector('.show-btn')
+            const hide_sms_btn = card.querySelector('.hide-btn')
+            show_sms_btn.addEventListener('click', () => {
+                message_container.classList.remove('hidden')
+                show_sms_btn.classList.add('hidden')
+                hide_sms_btn.classList.remove('hidden')
+            })
+            hide_sms_btn.addEventListener('click', () => {
+                message_container.classList.add('hidden')
+                show_sms_btn.classList.remove('hidden')
+                hide_sms_btn.classList.add('hidden')
+            })
             container.appendChild(card);
         });
     })
